8d83e8775a8a83e596bcadfa7ddf3bad857bf401
Anton <datskiiak@yandex.ru>
2024-08-14 16:58:31 +0400

    add script to save video by frames

--- detection/detector.py
+++ detection/detector.py
@@ -4,7 +4,7 @@ import os
 weights = r"trained_models\18k aero (batch 32)\weights\best.pt"
 model = YOLO(weights)
 
-basePath = r"test_data\\"
+basePath = r"test_data\\photos\\"
 imagePaths = [basePath + imagePath for imagePath in os.listdir(basePath)]
 model.predict(
     imagePaths, 
