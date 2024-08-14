430e23dffe6b7f6a2318d6af1758ec9c4480f657
Anton <datskiiak@yandex.ru>
2024-08-14 10:48:31 +0400

    add attributes, relative path

--- detection/detector.py
+++ detection/detector.py
@@ -1,9 +1,15 @@
 from ultralytics import YOLO 
 import os
   
-weights = r"F:\Всё\Linkos\navigation\detection\18k aero (batch 32)\weights\best.pt"
+weights = r"trained_models\18k aero (batch 32)\weights\best.pt"
 model = YOLO(weights)
 
-basePath = r"F:\Всё\Linkos\navigation\detection\test_data"
-imagePaths = [basePath + "\\" + imagePath for imagePath in os.listdir(basePath)]
-model.predict(imagePaths, save=True)
+basePath = r"test_data\\"
+imagePaths = [basePath + imagePath for imagePath in os.listdir(basePath)]
+model.predict(
+    imagePaths, 
+    save=True,
+    iou=0.4,
+    show_labels=True,
+    show_conf=False,
+    )
